eebd549620b171746a65e1444d47723c64cddb59
KyleSimmonsC44 <darrensimmons92@gmail.com>
2020-12-09 14:21:40 -0600

    added buttons to the orders

--- src/orders/Orders.js
+++ src/orders/Orders.js
@@ -2,11 +2,19 @@ import React from "react"
 import "./Order.css"
 
 export const Order = ({order, bakedGood, user}) =>(
-    <div className="orders">
+    <div className="order">
         <h3>You have an order!</h3>
         <div>Submitted by: {user.firstName} {user.lastName}</div>
 <div>They want: {bakedGood.name === "cake" ? bakedGood.name : order.quantity + " " + bakedGood.name} </div>
         <div>Description: {order.description}</div>
         <div>Date Needed By: {order.dateNeededBy}</div>
+        <div className="buttons">
+            <button>
+                Accept
+            </button>
+            <button>
+                Deny
+            </button>
+        </div>
     </div>
 )
